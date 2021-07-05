7cab558e9fd025ac8642f9746306ccafb5f3ec23
ilyassmoutite <45095556+ilyassmoutite@users.noreply.github.com>
2021-07-05 12:09:52 +0100

    Update standardizedBuild.groovy

--- vars/standardizedBuild.groovy
+++ vars/standardizedBuild.groovy
@@ -34,7 +34,7 @@ def call(String nodeName = 'build') {
 
 
         stage('\u2795 Pushing Artifacts') {
-         //   sh 'aws s3 cp ' + OUTPUT_FILE + ' s3://' + params.S3_DEST_BUCKET + '/jobs/' + JOB_NAME + '/' + BUILD_NUMBER + '/' + OUTPUT_FILE
+            sh 'aws s3 cp ' + OUTPUT_FILE + ' s3://' + params.S3_DEST_BUCKET + '/jobs/' + JOB_NAME + '/' + BUILD_NUMBER + '/' + OUTPUT_FILE
             currentBuild.result = "SUCCESS"
         }
     }
